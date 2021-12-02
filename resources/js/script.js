cd4289349e596665ee202d1aabcd97b62dc1b228
theNodeNinja <mgwinhtetnaing02@gmail.com>
2021-12-01 18:19:59 -0600

    responsive

---
+++ resources/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+  // sticky nav
+
+  //scroll
+  $(".js--btn-hungry").click(function () {
+    $("html,body").animate(
+      { scrollTop: $(".js--priceSection").offset().top },
+      3000
+    );
+  });
+
+  $(".js--btn-more").click(function () {
+    $("html,body").animate({ scrollTop: $(".js--feature").offset().top }, 1000);
+  });
+
+  // smooth scrolling
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+
+  // animation scroll
+  $(".feature-body").waypoint(
+    function (direction) {
+      $(".feature-body").addClass("animated fadeInUpBig");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".app").waypoint(
+    function (direction) {
+      $(".app").addClass("animated fadeInUpBig");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".cityRow").waypoint(
+    function (direction) {
+      $(".cityRow").addClass("animated fadeInUpBig");
+    },
+    {
+      offset: "50%",
+    }
+  );
+});
